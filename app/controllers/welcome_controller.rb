ee19700c5c3a73885c52bbe754253cb379d62261
Jan Schulte <hello@unexpected-co.de>
2014-11-02 17:40:34 +0100

    make load_all method static

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
 
-    @attendees = AttendeeLoader.new.load_all
+    @attendees = AttendeeLoader.load_all
   end
 end
--- app/lib/attendee_loader.rb
+++ app/lib/attendee_loader.rb
@@ -1,5 +1,5 @@
 class AttendeeLoader
-  def load_all
+  def self.load_all
     all_files = Dir.glob(Rails.root.join('config', 'attendees', '*.yaml'))
     all_files.map do |file|
       hash = YAML.load_file(file)
