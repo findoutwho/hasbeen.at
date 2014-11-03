7f5c34fe4eb7243a2f7fda86cd08e2c77968e4f4
Jan Schulte <hello@unexpected-co.de>
2014-11-03 20:59:52 +0100

    Check if id exists

--- app/lib/attendee_loader.rb
+++ app/lib/attendee_loader.rb
@@ -7,7 +7,12 @@ class AttendeeLoader
     end
   end
 
+  def self.exists?(id)
+    File.exists?(Rails.root.join('config', 'attendees', "#{id}.yaml"))
+  end
+
   def self.by_id(id)
+    return nil unless exists?(id)
     attendee_file = Dir.glob(Rails.root.join('config', 'attendees', "#{id}.yaml"))
     Attendee.new(YAML.load_file(attendee_file.first))
   end
