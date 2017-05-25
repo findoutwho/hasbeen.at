96169e333523ee23fb9644af449e0b6fd44eef05
Jan Schulte <hello@unexpected-co.de>
2017-05-25 22:22:59 +0200

    Listen on 127.0.0.1:3000

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,7 +10,7 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "127.0.0.1:3000", :backlog => 64
 
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
